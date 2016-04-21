22ec63a7fdcae67d662d6496bc69487056a746f2
brianorina <orinabrian100@gmail.com>
2016-04-21 19:08:13 +0300

    second commit

---
+++ variable1.cpp
@@ -0,0 +1,35 @@
+oopii@oopiibox:~$ ls
+a.out      Hello world             oop2      Templates             va.txt
+Desktop    Helloworld.cpp          Pictures  variables2.cpp        Videos
+Documents  Hello world output.txt  Public    variables.cpp
+Downloads  Music                   readme    variables.cpp.output
+oopii@oopiibox:~$ g++ variables2.cpp
+oopii@oopiibox:~$ ./a.out
+
+^C
+oopii@oopiibox:~$ ls
+a.out      Hello world             oop2      Templates       variables.cpp.output
+Desktop    Helloworld.cpp          Pictures  variable1.cpp   va.txt
+Documents  Hello world output.txt  Public    variables2.cpp  Videos
+Downloads  Music                   readme    variables.cpp
+oopii@oopiibox:~$ g++ variables1.cpp
+g++: error: variables1.cpp: No such file or directory
+g++: fatal error: no input files
+compilation terminated.
+oopii@oopiibox:~$ g++ variable1.cpp
+oopii@oopiibox:~$ ./a.out
+2 12oopii@oopiibox:~$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int x=12;
+
+
+cout << x / 3 << ' ' << x * 2;
+
+return 0;
+}
